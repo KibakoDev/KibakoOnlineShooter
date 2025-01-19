1b711785d1a8df40902e7e26bf33025fc851b75e
Kibako Games <kibakodev@gmail.com>
2025-01-19 18:32:53 -0500

    Update Source

--- Source/KibakoOnlineShooter/KibakoOnlineShooter.Build.cs
+++ Source/KibakoOnlineShooter/KibakoOnlineShooter.Build.cs
@@ -11,6 +11,8 @@ public class KibakoOnlineShooter : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
